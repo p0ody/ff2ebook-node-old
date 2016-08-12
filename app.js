66b2ec8d9b26cadf93884952fd818207168b0f31
p0ody <mcliche91@gmail.com>
2016-08-12 13:13:20 -0400

    Removed testing url

--- app.js
+++ app.js
@@ -55,10 +55,11 @@ app.use('/archive', archive);
 app.use('/direct', direct);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function (req, res, next)
+{
+    res.status(404).send("404 error: Page not found.");
     var err = new Error('Not Found');
     err.status = 404;
-    next(err);
 });
 
 // error handlers
